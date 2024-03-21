7416e43907ce1d56a007d98c36949154d99fd5e9
SammyMertz <juliemontalbo0cc5ad4731290@outlook.com>
2024-03-22 05:19:16 +0800

    ut

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const prettierConfig = {
+    useTabs: false,
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    printWidth: 200,
+};
+
+module.exports = prettierConfig;
